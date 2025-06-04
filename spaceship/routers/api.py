9b37a3c91ccf617511f7cc9ce4d5d54088be0da2
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-06-04 13:32:35 +0300

    Add numpy dependencie and new end point

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+import numpy as np  
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix-multiply')
+def matrix_multiply() -> dict:
+    matrix_a = np.random.randint(0, 10, size=(10, 10)).tolist()
+    matrix_b = np.random.randint(0, 10, size=(10, 10)).tolist()
+
+    product = (np.array(matrix_a) @ np.array(matrix_b)).tolist()
+
+    return {
+        'matrix_a': matrix_a,
+        'matrix_b': matrix_b,
+        'product': product
+    }
